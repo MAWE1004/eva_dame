6bfabd65871652ea883f9ba1aa768724c5a585be
MAWE1004 <93674083+MAWE1004@users.noreply.github.com>
2023-01-13 19:58:47 +0100

    sendGameOver erstellt um Mitspieler bescheid zu geben

---
+++ dame_projekt/src/socket/SendGameOver.java
@@ -0,0 +1,35 @@
+package socket;
+
+import java.nio.ByteBuffer;
+
+public class SendGameOver {
+    private byte code;
+
+    public SendGameOver(){
+    }
+
+    public SendGameOver(byte code){
+        this.code = code;
+    }
+
+    public byte[] marshall(){
+        byte[] buffer;
+        ByteBuffer out = ByteBuffer.allocate(5);
+        String value = "time";
+        out.put(value.getBytes());
+        out.put(code);
+        buffer = out.array();
+        return buffer;
+    }
+
+    public SendGameOver unMarshall(byte[] req){
+        ByteBuffer in = ByteBuffer.wrap(req);
+        byte[] sin = new byte[4];
+        in.get(sin, 0, 4);
+        if(!"time".equals(new String(sin)))
+            return null;
+
+        code = in.get();
+        return this;
+    }
+}

d8c7a3e59c4899eb734c93b681d1ca5d6e686fd9
marcelmartin1 <mama1001@stud.hs-kl.de>
2023-01-18 13:03:04 +0100

    Fixed Server Bug

--- dame_projekt/src/controller/menu/MenuController.java
+++ dame_projekt/src/controller/menu/MenuController.java
@@ -115,6 +115,8 @@ public class MenuController implements ActionListener {
             int port = 1235;
             MulticastSocket socket = new MulticastSocket(port);
             socket.setSoTimeout(5000);
+            InetAddress serverAdr = InetAddress.getByName("10.0.3.36");
+            socket.setInterface(serverAdr);
             socket.joinGroup(gruppe);
             byte[] buffer = new byte[100];
             DatagramPacket receive = new DatagramPacket(buffer, buffer.length);
